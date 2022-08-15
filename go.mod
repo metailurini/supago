7e6f879b32e557b37683609b2cf06f9108514519
Shánenoi <shanenoi.org@gmail.com>
2022-08-15 23:58:41 +0700

    add config wrapper for postgresql

--- go.mod
+++ go.mod
@@ -3,14 +3,13 @@ module github.com/metailurini/supago
 go 1.18
 
 require (
-	github.com/hasura/go-graphql-client v0.7.2
 	github.com/jackc/pgx/v4 v4.16.1
-	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c
+	github.com/spf13/viper v1.12.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -18,11 +17,19 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.11.0 // indirect
-	github.com/klauspost/compress v1.10.3 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
-	nhooyr.io/websocket v1.8.7 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,24 +1,109 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"flag"
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/jackc/pgx/v4"
+	"github.com/spf13/viper"
 )
 
-func main() {
-	/*
-		goto https://app.supabase.com/project/<project-id>/settings/database
-		database URI will be like postgresql://<user-name>:<password>@db.<project-id>.supabase.co:5432/postgres
-	*/
-	var dbFlag string
-	flag.StringVar(&dbFlag, "db", "", "database URI")
-	flag.Parse()
+// load from env
+// override config from env
+// return core package
 
-	conn, err := pgx.Connect(context.Background(), dbFlag)
+var yamlExample = []byte(`
+Hacker: true
+name: steve
+hobbies:
+- skateboarding
+- snowboarding
+- go
+clothing:
+  jacket: leather
+  trousers: denim
+age: 35
+eyes : brown
+beard: true
+postgres: postgresql://postgres:this13dewevwk454f3f25424523f@db.ddhcxdcfqdhkyxrbpfnh.supabase.co:5432/postgres
+`)
+
+type SetupWrapper interface {
+	LoadConfig(cfg Config) error
+	Apply(setup func(Config) Config)
+	CoreValue() interface{}
+}
+
+type Config interface {
+	Get(key string) interface{}
+	Set(key string, value interface{})
+}
+
+type Postgre struct {
+	conn *pgx.Conn
+}
+
+func (p *Postgre) LoadConfig(cfg Config) error {
+	ctx, ok := cfg.Get("context").(context.Context)
+	if !ok {
+		return errors.New("can not get context")
+	}
+
+	uri, ok := cfg.Get("postgres").(string)
+	if !ok {
+		return errors.New("can not get uri")
+	}
+
+	conn, err := pgx.Connect(ctx, uri)
+	if err != nil {
+		return err
+	}
+
+	p.conn = conn
+	return nil
+}
+
+func (p *Postgre) Apply(setup func(Config) Config) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (p *Postgre) CoreValue() interface{} {
+	return p.conn
+}
+
+type ViperWrapper struct{}
+
+func (v *ViperWrapper) Get(key string) interface{} {
+	return viper.Get(key)
+}
+
+func (v *ViperWrapper) Set(key string, value interface{}) {
+	viper.Set(key, value)
+}
+
+func main() {
+	viper.SetConfigType("yaml") // or viper.SetConfigType("YAML")
+	viper.Set("context", context.Background())
+
+	// any approach to require this configuration into your program.
+
+	err := viper.ReadConfig(bytes.NewBuffer(yamlExample))
+	if err != nil {
+		panic(err)
+	}
+
+	p := Postgre{}
+	if err := p.LoadConfig(&ViperWrapper{}); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("viper.GetString(\"postgres\"): %v\n", viper.GetString("postgres"))
+
+	conn := p.CoreValue().(*pgx.Conn)
+	// conn, err := pgx.Connect(context.Background(), viper.GetString("postgres"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
